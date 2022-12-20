38a50325c896ae710fe4311db564b32a70fdcc1f
Mark Lannen <mark.lannen@mso.umt.edu>
2022-12-19 18:06:09 -0700

    add react router, routes and style navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 
-
 const Shop = () => {
-  return <h1>Hi I am the Shop Page</h1>
+  return(
+    <h1>Hi I am the Shop Page</h1>
+  ) 
 }
 
 const App = () => {
   return (
     <Routes>  
-        <Route path='/' element={<Home />} /> 
-        <Route path='/shop' element={<Shop />} /> 
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />}/> 
+        <Route path='shop' element={<Shop />} /> 
+      </Route>  
     </Routes>
   );
 }
